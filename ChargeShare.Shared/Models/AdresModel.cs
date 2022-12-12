e2039d34d6ea68162803ce02cc5459c32519b751
JustFalco <falco@wolkorte.nl>
2022-12-12 17:44:57 +0100

    added second test prop for testing submodules

--- ChargeShare.Shared/Models/AdresModel.cs
+++ ChargeShare.Shared/Models/AdresModel.cs
@@ -19,5 +19,6 @@ public class AdresModel
     public string HouseNumber { get; set; }
     public string HouseAddition { get; set; }
     public string TestProp { get; set; }
+    public string TestProp2 { get; set; }
 }
 
